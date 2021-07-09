86a5d05374010e2a49654aed684a00354cf8e55d
slothdabski <allthatsleft169@gmail.com>
2021-07-09 18:54:34 -0500

    dotOS
    Change-Id: Icc24d05364350af35df5c71ad643bc17adb9d8d8

--- dot_guacamole.mk
+++ dot_guacamole.mk
@@ -12,16 +12,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/guacamole/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
+
+# Inherit GApps if exsits
+$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1440
 
-#Official Tag
-DERP_BUILDTYPE := Official
-
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := derp_guacamole
+PRODUCT_NAME := dot_guacamole
 PRODUCT_DEVICE := guacamole
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := GM1917
@@ -32,14 +32,22 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 PRODUCT_CHARACTERISTICS := nosdcard
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 3120
-TARGET_SCREEN_WIDTH := 1440
+TARGET_BOOT_ANIMATION_RES := 1440
 
 # Build info
-BUILD_FINGERPRINT := "OnePlus/OnePlus7Pro/OnePlus7Pro:10/QKQ1.190716.003/1909110008:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus7Pro \
     PRODUCT_NAME=OnePlus7Pro \
     PRIVATE_BUILD_DESC="OnePlus7Pro-user 10 QKQ1.190716.003 1909110008 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+# Extras
+EXTRA_FOD_ANIMATIONS := true
+TARGET_SUPPORTS_BLUR := true
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+USE_GAPPS=true
+TARGET_INCLUDE_AOSP_REPLACEMENT := true
